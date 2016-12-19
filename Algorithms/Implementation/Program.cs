3b6963a1c55afbe98ce92fa6bc9c6ef49c2daff7
Carlos Figueira <carlosfigueirafilho@hotmail.com>
2016-12-18 18:08:56 -0800

    One more

--- Algorithms/Implementation/Program.cs
+++ Algorithms/Implementation/Program.cs
@@ -10,7 +10,44 @@ namespace Implementation
     {
         static void Main(string[] args)
         {
-            UtopianTree.Run();
+            //UtopianTree.Run();
+            SherlockAndSquares.Run();
+        }
+    }
+
+    // https://www.hackerrank.com/challenges/sherlock-and-squares
+    class SherlockAndSquares
+    {
+        public static void Run()
+        {
+            int t = int.Parse(Console.ReadLine());
+            for (int i = 0; i < t; i++)
+            {
+                int[] ab = Array.ConvertAll(Console.ReadLine().Trim().Split(' '), int.Parse);
+                int a = ab[0];
+                int b = ab[1];
+                int sra = (int)Math.Sqrt(a);
+                int count = 0;
+                if (sra * sra == a)
+                {
+                    count++;
+                    sra++;
+                }
+
+                if (a != b)
+                {
+                    int srb = (int)Math.Sqrt(b);
+                    if (srb * srb == b)
+                    {
+                        count++;
+                        srb--;
+                    }
+
+                    count += (srb - sra);
+                }
+
+                Console.WriteLine(count);
+            }
         }
     }
 
