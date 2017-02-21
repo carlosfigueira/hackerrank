26776e14aca6311647725fbce3b58ed57fe04c49
Carlos Figueira <carlosfigueirafilho@hotmail.com>
2017-02-21 06:02:21 -0800

    Week of code 29 - day 2

---
+++ Contests/WeekOfCode29/BigSorting.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeekOfCode29
+{
+    class BigSorting
+    {
+        public static void Run()
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] numbers = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = Console.ReadLine();
+            }
+
+            Array.Sort(numbers, (n1, n2) =>
+            {
+                if (n1.Length != n2.Length)
+                {
+                    return n1.Length - n2.Length;
+                }
+
+                for (int i = 0; i < n1.Length; i++)
+                {
+                    if (n1[i] != n2[i])
+                    {
+                        return (n1[i] - n2[i]);
+                    }
+                }
+
+                return 0;
+            });
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+}
