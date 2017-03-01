10d036770cd705437acdae0f7a77f7a86da9aa5f
Carlos Figueira <carlosfigueirafilho@hotmail.com>
2017-03-01 09:10:38 -0800

    Scripts for linear algebra foundations

---
+++ Mathematics/LinearAlgebraFoundations/Scripts.R
@@ -0,0 +1,36 @@
+laf1 <- function() {
+  a <- matrix(c(1, 2, 3, 2, 3, 4, 1, 1, 1), nrow = 3, byrow = T)
+  b <- matrix(c(4, 5, 6, 7, 8, 9, 4, 5, 7), nrow = 3, byrow = T)
+  print (a + b)
+}
+
+laf2 <- function() {
+  a <- matrix(c(1, 2, 3, 2, 3, 4, 1, 1, 1), nrow = 3, byrow = T)
+  b <- matrix(c(4, 5, 6, 7, 8, 9, 4, 5, 0), nrow = 3, byrow = T)
+  print (a - b)
+}
+
+laf3 <- function() {
+  a <- matrix(c(1, 2, 2, 3), nrow = 2, byrow = T)
+  b <- matrix(c(4, 5, 7, 8), nrow = 2, byrow = T)
+  print (a %*% b)
+}
+
+laf4 <- function() {
+  a <- matrix(c(1, 2, 3, 2, 3, 4, 1, 1, 1), nrow = 3, byrow = T)
+  b <- matrix(c(4, 5, 6, 7, 8, 9, 4, 5, 7), nrow = 3, byrow = T)
+  print (a %*% b)
+}
+
+laf5 <- function() {
+  a <- matrix(c(1, 1, 0, 0, 1, 0, 0, 0, 1), nrow = 3, byrow = T)
+  b <- a
+  for (i in 2:100) {
+    b <- b %*% a
+  }
+  print (b[1,1])
+  print (b[1,2])
+  print (b[2,2])
+  print (b[3,2])
+  print (b[3,3])
+}
