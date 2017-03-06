91497947b455c363137f09a0e92b42692636f926
Carlos Figueira <carlosff@microsoft.com>
2017-03-06 12:41:32 -0800

    Linear algebra

--- Mathematics/LinearAlgebraFoundations/Scripts.R
+++ Mathematics/LinearAlgebraFoundations/Scripts.R
@@ -43,3 +43,53 @@ laf7 <- function() {
   }
   print (b)
 }
+
+laf9 <- function() {
+  # https://www.hackerrank.com/challenges/linear-algebra-fundamentals-9-eigenvalues
+  a <- matrix(c(0, 1, -2, -3), nrow = 2, byrow = TRUE)
+  eigen(a, only.values = TRUE)$values
+}
+
+laf10 <- function() {
+  # https://www.hackerrank.com/challenges/linear-algebra-fundamentals-10-eigenvectors
+  a <- matrix(c(0, 1, -2, -3), nrow = 2, byrow = TRUE)
+  eigen(a)$vectors
+}
+
+detMatrix1 <- function() {
+  # https://www.hackerrank.com/challenges/determinant-of-the-matrix-1
+  a <- matrix(c(3,0,0,-2,4, 0,2,0,0,0, 0,1,0,5,-3, -4,0,1,0,6, 0,-1,0,3,2), nrow = 5, byrow = TRUE)
+  d <- det(a)
+  print(d)
+}
+
+eigen1 <- function() {
+  # https://www.hackerrank.com/challenges/eigenvalue-of-matrix-1
+  a <- matrix(c(1,-3,3, 3,-5,3, 6,-6,4), nrow = 3, byrow = TRUE)
+  eigen(a, only.values = TRUE)$values
+}
+
+eigen2 <- function() {
+  # https://www.hackerrank.com/challenges/eigenvalue-of-matrix-2
+  a <- matrix(c(1,2, 2,4), nrow = 2, byrow = TRUE)
+  eigen(a, only.values = TRUE)$values
+}
+
+eigen3 <- function() {
+  # https://www.hackerrank.com/challenges/eigenvalue-of-matrix-3
+  a <- matrix(c(2,-1, -1,2), nrow = 2, byrow = TRUE)
+  eigen(a, only.values = TRUE)$values
+  eigen(a%*%a, only.values = TRUE)$values
+}
+
+eigen4 <- function() {
+  # https://www.hackerrank.com/challenges/eigenvalue-of-matrix-3
+  a <- matrix(c(2,-1, -1,2), nrow = 2, byrow = TRUE)
+  I <- matrix(c(1,0, 0,1), nrow = 2, byrow = TRUE)
+  library(MASS)
+  a_inv <- ginv(a)
+  eigen(a_inv, only.values = TRUE)$values
+  b <- a - 4*I
+  eigen(b, only.values = TRUE)$values
+}
+
